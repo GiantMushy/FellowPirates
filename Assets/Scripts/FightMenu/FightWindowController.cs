2d17be1523723c85336574ecb5046f1d5f4cab40
evaxrut <eva.rut.omarsdottir@gmail.com>
2025-12-01 19:28:18 +0000

    base for timing bar mini game

---
+++ Assets/Scripts/FightMenu/FightWindowController.cs
@@ -0,0 +1,51 @@
+// using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FightWindowController : MonoBehaviour
+{
+
+    //     private void Show()
+    //     {
+
+    //     }
+
+    //     public void ActivateAttackWindow()
+    //     {
+
+    //     }
+
+    //     public void ActivateItemsWindow()
+    //     {
+
+    //     }
+
+    //     public void ActivateBribeWindow()
+    //     {
+
+    //     }
+
+    //     public void ActivateFleekWindow()
+    //     {
+
+    //     }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "FightMiniGame") return;
+
+        GameObject root = scene.GetRootGameObjects()[0];
+        root.transform.SetParent(transform, false); // attach to purple panel
+    }
+}
