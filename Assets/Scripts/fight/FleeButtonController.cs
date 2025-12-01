d327e07f40b315d6ba77e6b4b9968fca1296093c
evaxrut <eva.rut.omarsdottir@gmail.com>
2025-12-01 17:58:51 +0000

    buttons finally work as expected

---
+++ Assets/Scripts/fight/FleeButtonController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FleeButtonController : MonoBehaviour
+{
+    [SerializeField] private Button fleeButton;
+
+    public void Flee()
+    {
+        Debug.Log("Flee button pressed");
+        EventSystem.current.SetSelectedGameObject(fleeButton.gameObject);
+
+    }
+}
