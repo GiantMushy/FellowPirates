064cc38960a142b09d524fa36746e32752742e58
evaxrut <eva.rut.omarsdottir@gmail.com>
2025-12-01 17:58:51 +0000

    buttons finally work as expected

--- Assets/Scripts/fight/AttackButtonController.cs
+++ Assets/Scripts/fight/AttackButtonController.cs
@@ -1,24 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 // using UnityEngine.SceneManagement;
 
 public class AttackButtonController : MonoBehaviour
 {
+    private Button attackButton;
+
+    // void Awake()
+    // {
+    //     attackButton = GetComponent<Button>();
+    // }
 
     public void Attack()
     {
+        // if (attackButton == null)
+        // {
+        //     Debug.LogWarning("attack button is null ");
+        // }
         Debug.Log("Attack button pressed");
 
+
+        // EventSystem.current.currentSelectedGameObject;
+        EventSystem.current.SetSelectedGameObject(EventSystem.current.currentSelectedGameObject);
+
+
+
     }
-    // // Start is called once before the first execution of Update after the MonoBehaviour is created
-    // void Start()
-    // {
 
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-
-    // }
 }
