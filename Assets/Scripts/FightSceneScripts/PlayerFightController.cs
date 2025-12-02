fbe21d3e67adf413c1c57c326e402dc9e86ac02a
evaxrut <eva.rut.omarsdottir@gmail.com>
2025-12-02 15:16:31 +0000

    on trigger enter 2D

--- Assets/Scripts/FightSceneScripts/PlayerFightController.cs
+++ Assets/Scripts/FightSceneScripts/PlayerFightController.cs
@@ -3,35 +3,40 @@ using UnityEngine;
 public class PlayerFightController : MonoBehaviour
 {
     public float speed = 0.1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            Debug.Log("up");
             transform.position = new UnityEngine.Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("down");
             transform.position = new UnityEngine.Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("right");
             transform.position = new UnityEngine.Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("left");
             transform.position = new UnityEngine.Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
 
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            Debug.Log("OnTriggerEnter with: " + other.name + " (tag: " + other.tag + ")");
+
+        if (other.CompareTag("Bomb"))
+        {
+            Debug.Log("collided with boooomb!!");
         }
     }
 }
