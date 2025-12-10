6c882410b448f5aa50fa8ce693b4bf6bbc2e14e8
evaxrut <eva.rut.omarsdottir@gmail.com>
2025-12-10 17:10:42 +0000

    diff message on disabled heal depending on oranges

--- Assets/Scripts/FightMenu/ItemsButtonController.cs
+++ Assets/Scripts/FightMenu/ItemsButtonController.cs
@@ -62,7 +62,7 @@ public class ItemsButtonController : MonoBehaviour
 
             if (gameManager.healthInventory <= 0 || gameManager.health > 2)
             {
-                attackFlow.ShowItemsMessageFullHealth();
+                attackFlow.ShowItemsMessageDisabled();
             }
             else
             {
