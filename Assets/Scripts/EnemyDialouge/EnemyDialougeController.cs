3b82bfd6f38002092427f5a4b7adc16e2036ceb7
Þorri Bjarnason <thorvardur23@ru.is>
2025-12-12 10:51:38 +0000

    Stafsetningavilla

--- Assets/Scripts/EnemyDialouge/EnemyDialougeController.cs
+++ Assets/Scripts/EnemyDialouge/EnemyDialougeController.cs
@@ -119,10 +119,10 @@ public class EnemyDialougeController : MonoBehaviour
         DialougeBox.color = Hex("#FF7A7A");
 
         lines = new string[]
-              {
-            "Ye dare distrub me on my rum break!",
+        {
+            "Ye dare disturb me on my rum break!",
             "Your gold is mine landlubber!"
-       };
+        };
         EnemyNameText.color = Color.white;
         StartDialouge();
     }
